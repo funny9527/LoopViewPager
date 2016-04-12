25955ad377c4d32190ae4891ff39d004ab06e750
szy <jinfengox@126.com>
2016-04-13 00:27:01 +0800

    add base code

---
+++ src/com/example/pagertest/Pager.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.example.pagertest;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+/**
+ * @author szy
+ *
+ */
+public class Pager extends LinearLayout {
+
+	public Pager(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override 
+	public boolean onTouchEvent(MotionEvent event) {
+		return true;
+	}
+
+}
